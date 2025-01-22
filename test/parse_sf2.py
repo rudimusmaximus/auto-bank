c2fdfb2312cc720da344f5157b8b2d00603a9051
raul flores jr <rudy@redcrowconsulting.com>
2025-01-22 17:22:17 -0600

    feat: v0.1.1-initial-code.3 ready for remote test
    - push a release out and test on user machine
    
     - todo if working, work out notes for user to call from anywhere on
    machine without placing the scripts in the same target directories with
    the sf2 files in them

--- test/parse_sf2.py
+++ test/parse_sf2.py
@@ -14,26 +14,18 @@ def main():
     try:
         with open(sf2_path, 'rb') as sf2_file:
             sf2 = Sf2File(sf2_file)
-            # Create a list of dicts with the needed fields
+
             presets_info = []
             for p in sf2.presets:
-                # Check if 'preset' and 'bank' exist
                 if hasattr(p, 'preset') and hasattr(p, 'bank'):
                     presets_info.append({
                         "name": p.name,
                         "preset": p.preset,
                         "bank": p.bank,
                     })
-                else:
-                    # This might be a special placeholder or "EOP" object.
-                    # You can log or silently skip it.
-                    pass
-
-            # Print JSON to stdout
             print(json.dumps(presets_info))
 
     except Exception as e:
-        # On any error, print to stderr and exit 1
         print(str(e), file=sys.stderr)
         sys.exit(1)
 
